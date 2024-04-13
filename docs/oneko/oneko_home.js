c34b010ec18a0a8bbc6c9b7c97fa6dee606e8d08
jalog <jaro.loge@ugent.be>
2024-04-13 20:04:04 +0200

    reordered oneko files

--- docs/oneko/oneko_home.js
+++ docs/oneko/oneko_home.js
@@ -96,7 +96,7 @@
         nekoEl.style.top = `${nekoPosY - 16}px`;
         nekoEl.style.zIndex = Number.MAX_VALUE;
 
-        let nekoFile = "images/oneko.gif"
+        let nekoFile = "oneko/oneko.gif"
         const curScript = document.currentScript
         if (curScript && curScript.dataset.cat) {
             nekoFile = curScript.dataset.cat
