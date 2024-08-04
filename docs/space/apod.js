857ac2b5bbac3626267fc5f2a4239010c155e324
jalog <jaro.loge@ugent.be>
2024-08-04 16:04:26 +0200

    displays apod videos

--- docs/space/apod.js
+++ docs/space/apod.js
@@ -4,17 +4,27 @@ let apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 let currentDate = new Date();
 
 function fetchApod(date) {
+    $('#apod-image').hide();
+    $('#apod-video').hide();
+
     apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date.toISOString().split('T')[0]}`;
     $.ajax({
         url: apiUrl,
         method: 'GET',
         success: function (data) {
-            const imageUrl = data.url;
             const title = data.title;
             const date = data.date;
             const explanation = data.explanation;
 
-            $('#apod-image').attr('src', imageUrl);
+            let url;
+            if (data.media_type === "image") {
+                url = data.hdurl;
+                $('#apod-image').attr('src', url).show();
+            } else {
+                url = data.url;
+                $('#apod-video').attr('src', url).show();
+            }
+
             $('#apod-title').text(title);
             $('#apod-date').text(date);
             $('#apod-explanation').text(explanation);
